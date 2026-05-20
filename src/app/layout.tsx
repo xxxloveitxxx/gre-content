e1bade9215632b3e7e3be48c731b1a828b68dc32
xxxloveitxxx <socialorbit1@gmail.com>
2026-05-20 18:36:17 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from 'next';
-import 'app/globals.css';
+import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import ClientLayout from './ClientLayout';
 
