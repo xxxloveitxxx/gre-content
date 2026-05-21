42edb4a227fef34b9d60ecb9c9ccf077707d338f
xxxloveitxxx <socialorbit1@gmail.com>
2026-05-21 18:14:26 +0100

    Delete src/ai/genkit.ts

--- src/ai/genkit.ts
+++
@@ -1,9 +0,0 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
-
-export const ai = genkit({
-  plugins: [googleAI({
-    apiKey: process.env.NEXT_PUBLIC_GOOGLE_GENAI_API_KEY || process.env.GOOGLE_GENAI_API_KEY
-  })],
-  model: 'googleai/gemini-2.5-flash',
-});
